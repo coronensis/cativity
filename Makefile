705ea61645bd655de48df2d499a9980d8c01db54
Helmut Sipos <helmut.sipos@gmail.com>
2025-12-13 18:17:06 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,95 @@
+#
+# CatIVity - CI-V (CAT protocol) based VFO tuner for the Xiegu G90 transceiver
+#
+# Makefile: MAKE(1) control file to build the software and download
+#           it to the microcontroller
+#
+# Copyright (c) 2025 Helmut Sipos, YO6ASM <yo6asm@gmail.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# MCU clock frequency
+F_CPU = 8000000
+
+# Flags for C files
+CFLAGS += -std=gnu11
+CFLAGS += -ffreestanding -Wall -Wextra -Werror -Wundef -Wshadow \
+	  -Wunused-parameter -Warray-bounds -Wstrict-prototypes -Wredundant-decls \
+	  -Wbad-function-cast -Wunreachable-code
+CFLAGS += -gdwarf-2
+CFLAGS += -mmcu=atmega328p
+CFLAGS += -Os -mcall-prologues
+CFLAGS += -DF_CPU=$(F_CPU)
+
+# Linker flags
+LDFLAGS += -Wl,-Map,cativity.map
+
+# Source code files
+CSRC = cativity.c
+
+# Default target
+all: cativity.hex
+
+# Compile the application into the image to be used for flashing.
+cativity.hex: $(CSRC)
+	avr-gcc --version
+	avr-gcc $(CFLAGS) $(CSRC) -o cativity.elf
+	avr-objcopy -j .text -j .data -j .eeprom -j .fuse -O ihex cativity.elf cativity.hex
+	avr-objcopy -j .text -j .data -O binary cativity.elf cativity.bin
+	avr-objdump -h -S -C cativity.elf > cativity.lst
+	avr-nm -n cativity.elf > cativity.sym
+	avr-size -A --mcu=atmega328p cativity.elf
+
+# Remove build artefacts
+clean:
+	rm -f *.o *.a *.elf *.hex *.bin *.lst *.sym trace*.txt
+
+# NOTE: The following pinctrl stuff only applies when running on a Raspberry Pi
+
+# Download the firmware to the microcontroller
+install: cativity.hex
+	pinctrl set 21 op
+	pinctrl set 21 dl
+	avrdude -V -p m328p -P /dev/spidev0.0:/dev/gpiochip0 -c linuxspi -B 10000 -U flash:w:cativity.hex
+	pinctrl set 21 dh
+
+# Read the fuse bits of the microcontroller
+rdfuse:
+	pinctrl set 21 op
+	pinctrl set 21 dl
+	avrdude -V -p m328p -P /dev/spidev0.0:/dev/gpiochip0 -c linuxspi -B 10000 -U lfuse:r:-:h -U hfuse:r:-:h -U efuse:r:-:h
+	pinctrl set 21 dh
+
+# Write the fuse bits of the microcontroller
+wrfuse:
+	pinctrl set 21 op
+	pinctrl set 21 dl
+	avrdude -p m328p -P /dev/spidev0.0:/dev/gpiochip0 -c linuxspi -B 10000 -U lfuse:w:0xE2:m -U hfuse:w:0xDD:m -U efuse:w:0xFF:m
+	pinctrl set 21 dh
+
+# HW reset the microcontroller
+reset:
+	pinctrl set 21 op
+	pinctrl set 21 dl
+	sleep 1
+	pinctrl set 21 dh
---
+++ cativity.c
@@ -0,0 +1,277 @@
+/*
+ * CI-V protocol based VFO controller for Xiegu G90 using an ATmega328p
+ *
+ * Features:
+ *  - CI-V GetFrequency for synchronization with the radio
+ *  - CI-V SetFrequency on potentiometer changes to tune 10 KHz, 1 KHz, 100 Hz and 10 Hz ranges
+ *
+ * cativity.c: Implementation
+ *
+ * Copyright (c) 2025 Helmut Sipos, YO6ASM <yo6asm@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdint.h>
+
+#define UART_BAUDRATE 19200
+
+#define ADC_CHANNEL_2 0x02
+#define ADC_CHANNEL_3 0x03
+
+#define LED_RED       PD2
+
+#define LED_OFF       0
+#define LED_ON        1
+
+// RX buffer
+volatile uint8_t uart_rx_buffer[64];
+// Counter indicating how many bytes are in the RX buffer
+volatile uint8_t uart_rx_bytes = 0x00;
+
+// Digitized voltage set by the voltage divider right potentiometer
+volatile uint8_t right_pot_value = 0x00;
+
+// Digitized voltage set by the voltage divider right potentiometer
+volatile uint8_t left_pot_value = 0x00;
+
+
+// UART service routine
+ISR(USART_RX_vect)
+{
+    // Copy the received byte inito the buffer if there is space left
+    // Increment the counter indicating how many bytes are in the buffer
+    if (uart_rx_bytes < sizeof(uart_rx_buffer)) {
+        uart_rx_buffer[uart_rx_bytes] = UDR0;
+        uart_rx_bytes++;
+    }
+}
+
+// ADC service routine
+ISR(ADC_vect)
+{
+    uint8_t adc_l = ADCL;
+    uint8_t adc_h = ADCH;
+    // The ADC conversion is a 10 bit value -> values range from 0...1024
+    // Diving by  10 puts it nicely into our desired range of 0..99
+    uint16_t adc_val = ((adc_h << 8) | adc_l) / 10;
+
+    // Clamp the value to 0..100
+    if (adc_val > 100) {
+        adc_val = 100;
+    }
+
+    // Alternatively read the value set by pot 1 and 2
+    if (ADMUX & 1) {
+        right_pot_value = (right_pot_value + adc_val)/2;
+        ADMUX = 0x40;
+    }
+    else {
+        left_pot_value = (left_pot_value + adc_val)/2;
+        ADMUX = 0x41;
+    }
+}
+
+// TIMER 1 service routine - this drives the reading of ADC
+ISR(TIMER1_OVF_vect)
+{
+    // Another interrupt will occur after ~50ms due to timer overflow
+    TCNT1L = 0xB0;
+    TCNT1H = 0x3C;
+}
+
+// Set red led to ON or OFF
+void led_red(uint8_t state)
+{
+    if (state == LED_ON) {
+        PORTD |= _BV(LED_RED);
+    }
+    else {
+        PORTD &= ~_BV(LED_RED);
+    }
+}
+
+// Send a byte through the UART
+void uart_send(const uint8_t *data, uint8_t length)
+{
+    for (int i = 0; i < length; ++i) {
+        // Wait for empty transmit buffer
+        while (bit_is_clear(UCSR0A, UDRE0))
+            ;
+
+        // Put tx data into the buffer. It will then be send by the hardware
+        UDR0 = data[i];
+    }
+}
+
+int main(void)
+{
+    uint8_t oneHzTenHz = 0x00;
+    uint8_t hundredHzOneKHz = 0x00;
+    uint8_t tenKHzHundredKHz = 0x00;
+    uint8_t oneMHzTenMHz = 0x00;
+
+    uint8_t last_right_pot_value = 0x00;
+    uint8_t last_left_pot_value = 0x00;
+
+    uint8_t timeout_counter = 0x00;
+
+    // Set CI-V specification for message details
+    const uint8_t cmdGetActiveVFOFrequency[] = {0xFE, 0xFE, 0x70, 0xE0, 0x03, 0xFD};
+    uint8_t cmdSetActiveVFOFrequency[] = {0xFE, 0xFE, 0x70, 0xE0, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFD};
+
+    // Disable interrupts while setting things up
+    cli();
+
+    // Initialize Ports
+    // Port B all inputs
+    DDRB  = 0x00;
+    // Pulls-ups for inputs
+    PORTB = 0xFF;
+
+    // Port C all inputs C0 and C1 -> ADC
+    DDRC  = 0x00;
+    // Enable pull-ups for uppermost 2 bits
+    PORTC = 0xF0;
+
+    // Port D - all inputs except UART TX and LED (PD1 and PD2)
+    DDRD  = 0x06;
+    // Enable internal pull-ups for all but RX and TX
+    PORTD = 0xF9;
+
+    // Initialize Timer1
+    TCCR1A = 0x00;
+    // 1:8 prescaler
+    TCCR1B = 0x02;
+    TCCR1C = 0x00;
+    // Enable TC1.ovf interrupt
+    TIMSK1 = 0x01;
+    // Interrupt after 50ms due to timer overflow
+    TCNT1L = 0xB0;
+    TCNT1H = 0x3C;
+
+    // Initialize UART set format N81, enable RX, TX and RX interrupt
+    UBRR0 = (uint8_t)((F_CPU / UART_BAUDRATE + 8) / 16 - 1);
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0); // Enable RX, TX, and RX interrupt
+
+    // Initialize ADC
+    // AVCC with external capacitor at AREF pin, align right, chanel 0
+    ADMUX = 0x40;
+
+    // Enable ADC, auto trigger, enable interrupt, prescaler = 64 -> result in 125KHz sampling rate
+    ADCSRA = 0xEE;
+
+    // ADC TIMER1 overflow mode. Free running mode would mean interrupts every 200us
+    ADCSRB = 0x06;
+    // Disable analog comp
+    ACSR   = 0x80;
+
+    // Disable ditital input for the analog input channels
+    DIDR0 |= (1 << ADC1D) | (1 << ADC0D);
+
+    // Enable interrupts while setting things up
+    sei();
+
+    // Forever
+    for (;;) {
+
+        // Turn the red LED on for 300 ms
+        led_red(LED_OFF);
+        _delay_ms(300);
+
+        // Do nothing while the potentiometers have not moved
+        if ((right_pot_value == last_right_pot_value)
+            &&
+            (left_pot_value == last_left_pot_value)
+           ) {
+            continue;
+        }
+
+        // Update last values
+        last_right_pot_value = right_pot_value;
+        last_left_pot_value = left_pot_value;
+
+        // Request the current frequency from the radio
+        uart_rx_bytes = 0;
+        uart_send(cmdGetActiveVFOFrequency, sizeof(cmdGetActiveVFOFrequency));
+
+        // Xiegu G90 is echoing back the received command and THEN sending the response
+        // therefore we need to wait for both messages.
+        timeout_counter = 50;
+        while ((uart_rx_bytes < 17) && --timeout_counter) {
+            _delay_ms(1);
+        }
+
+        // If we run into the timeout (i.e. no response from the tranceiver)
+        // keep the red LED on for another second then try again
+        if (timeout_counter == 0) {
+            led_red(LED_ON);
+            _delay_ms(1000);
+            continue;
+        }
+
+        // Check message for plausibility
+        if (    (uart_rx_buffer[6] == 0xFE)
+             && (uart_rx_buffer[7] == 0xFE)
+             && (uart_rx_buffer[8] == 0xE0)
+             && (uart_rx_buffer[9] == 0x70)
+             && (uart_rx_buffer[10] == 0x03)
+             && (uart_rx_buffer[16] == 0xFD)
+            ) {
+            tenKHzHundredKHz = uart_rx_buffer[13];
+            oneMHzTenMHz = uart_rx_buffer[14];
+        }
+        else {
+            // Malformed message -> keep the red led on for 3 seconds then try again
+            led_red(LED_ON);
+            _delay_ms(3000);
+            continue;
+        }
+
+        // Flash the red led
+        led_red(LED_ON);
+
+        oneHzTenHz = ((right_pot_value % 10) << 4) & 0xF0;
+        hundredHzOneKHz = (((left_pot_value % 10) << 4) | ((right_pot_value / 10) & 0x0F));
+        tenKHzHundredKHz &= 0xF0;
+        tenKHzHundredKHz |= ((left_pot_value / 10) & 0x0F);
+
+        cmdSetActiveVFOFrequency[5] = oneHzTenHz;
+        cmdSetActiveVFOFrequency[6] = hundredHzOneKHz;
+        cmdSetActiveVFOFrequency[7] = tenKHzHundredKHz;
+        cmdSetActiveVFOFrequency[8] = oneMHzTenMHz;
+
+        // Set the current frequency on the radio
+        uart_rx_bytes = 0;
+        uart_send(cmdSetActiveVFOFrequency, sizeof(cmdSetActiveVFOFrequency));
+
+        // Read (and ignore) the response to the set active vfo frequency command
+        timeout_counter = 50;
+        while ((uart_rx_bytes < 17) && --timeout_counter) {
+            _delay_ms(1);
+        }
+    }
+}
+
